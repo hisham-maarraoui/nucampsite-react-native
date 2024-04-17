10467c03b4babe5ff40646a947431db82c425975
Hisham <62486806+hisham-maarraoui@users.noreply.github.com>
2024-04-16 17:20:09 -0700

    Using Redux

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -5,16 +5,16 @@
 //   with instructors/classmates, generally as is expected from being a participant in the coding bootcamp.
 
 
-import { useState } from "react";
 import { ScrollView, Text } from 'react-native';
-import { PARTNERS } from '../shared/partners';
 import { Avatar, ListItem } from "react-native-elements";
 import { Card } from 'react-native-elements';
+import { useSelector } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
 
 
 
 const AboutScreen = () => {
-    const [partners, setPartners] = useState(PARTNERS);
+    const partners = useSelector((state) => state.partners);
 
     return (
         <ScrollView scrollIndicatorInsets={{ right: 1 }}>
@@ -22,18 +22,18 @@ const AboutScreen = () => {
             <Card>
                 <Card.Title>Community Partners</Card.Title>
                 <Card.Divider />
-                {partners.map((partner) => {
-                    return (
-
+                {partners.partnersArray.map((partner) => (
                     <ListItem key={partner.id}>
-                        <Avatar rounded source={partner.image}></Avatar>
+                        <Avatar
+                            rounded 
+                            source={{ uri: baseUrl + partner.image }} 
+                        />
                         <ListItem.Content>
                             <ListItem.Title>{partner.name}</ListItem.Title>
                             <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
                         </ListItem.Content>
                     </ListItem>
-                    )
-                })}
+                ))}
             </Card>
         </ScrollView>
     );
